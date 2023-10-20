df11c7ed56f4da23bff6c07e7b807e9cd816ed48
Roul <137962972+Ro0ul@users.noreply.github.com>
2023-10-20 09:48:24 +0100

    Updated The Routing , Added Middleware Support

--- src/Route.php
+++ src/Route.php
@@ -1,25 +1,59 @@
 <?php declare(strict_types=1);
 
+
 namespace RoulRouter;
 
+/**
+ * Route
+ */
 class Route 
 {
     private static array $routes = [];
-    public static function get(string $url, callable $callback) : void 
+    private static mixed $notFound = null;
+    /**
+     * Register a GET Route
+     */
+    public static function get(string $url, mixed $callback) : self 
     {
         static::$routes[] = [$url , $callback, "get"];
+        return new Route();
     }
-    public static function get(string $url, callable $callback) : void 
+     /**
+     * Register a POST Route
+     */
+    public static function post(string $url, mixed $callback) : self 
     {
         static::$routes[] = [$url , $callback, "post"];
+        return new Route();
     }
-    public static function get(string $url, callable $callback) : void 
-    {
-        static::$routes[] = [$url , $callback, "put"];
-    }
-    public static function get(string $url, callable $callback) : void 
+     /**
+     * Register a DELETE Route
+     */
+    public static function delete(string $url, mixed $callback) : self 
     {
         static::$routes[] = [$url , $callback, "delete"];
+        return new Route();
+    }
+     /**
+     * Register a PUT Route
+     */
+    public static function put(string $url, mixed $callback) : self 
+    {
+        static::$routes[] = [$url , $callback, "put"];
+        return new Route();
+    }
+
+    /**
+     * Adds A Middleware To Your Route
+     */
+    public static function middleware(string $name)
+    {
+        static::$routes[array_key_last(static::$routes)]["middleware"] = $name;
+
+    }
+    public static function setNotFound(mixed $callback) : void 
+    {
+        static::$notFound = $callback;  
     }
     public static function run() : mixed 
     {
@@ -30,12 +64,12 @@ class Route
         $params = [];
         foreach(static::$routes as $route){
             $uri = $route[0];
+            $uri = preg_replace('/{[^}]+}/',"(.+)",$uri);
             $method = $route[2];
             if($requestMethod != $method){
                 break;
             }
             $uri = trim($uri,"/");
-            
             if(preg_match("%^{$uri}$%",$requestUrl,$matches)){
                 $callback = $route[1];
                 unset($matches[0]);
@@ -44,9 +78,31 @@ class Route
             }
         }
         if($callback){
-            return call_user_func_array($callback,$params);
+            $middleware = $route["middleware"] ?? null;
+            if(!is_null($middleware)){
+                $class = $middleware;
+                $class = new $class();
+                return $class->handle($callback,$params); # You Can Modify How The function is called here 
+            }
+            if(is_callable($callback) or is_string($callback)){
+                return call_user_func($callback);  # You Can Modify How The function is called here 
+            }
+            if(is_array($callback)){
+                [$class, $method] = $callback;
+                $class = new $class;
+                return $class->$method(); # You Can Modify How The function is called here 
+            }
         }
-        echo "404";
-        return false;
+        if(!is_null(static::$notFound)){
+            if(is_callable(static::$notFound) or is_string(static::$notFound)){
+                return call_user_func(static::$notFound);  # You Can Modify How The function is called here 
+            }
+            if(is_array(static::$notFound)){
+                [$class, $method] = static::$notFound;
+                $class = new $class;
+                return $class->$method(); # You Can Modify How The function is called here 
+            }
+        }
+        throw new \Exception("404 Method Not Set");
     }
 }
